f0d8b752f5a6faacb34d62a31b43d0f00ab204bd
AndrzejSzeszko <andrzejgustaw@gmail.com>
2019-07-06 19:58:09 +0200

    argparse - make groups of arguments

--- src/main.py
+++ src/main.py
@@ -1,63 +1,78 @@
 #!/usr/bin/env python3
 import argparse
 
-from downloader import download_exercises
+from downloader import (
+    download_exercises, 
+    __get_all_tracks
+)
 from constants import (
     ALLOWED_STATUSES,
     ALLOWED_DIFFICULTIES,
     ALLOWED_GROUPS
 )
 
-description     = 'This is exercism-cli wrapper allowing to bulk download exercism exercises based on given conditions. \n\n' \
-                  '-------------------------------------------------------------------------------------------------------------------------\n' \
-                  '\n' \
-                  'Examples of use:\n' \
-                  '\n' \
-                  'exercism-bd -t python javascript -d easy   -->   downloads all easy exercises form python and javascript tracks \n' \
-                  '\n' \
-                  'exercism-bd -g side -s unlocked -e dummy@email.com -p dummypass   -->   downloads unlocked side exercises from all tracks \n' \
-                  '\n' \
-                  '-------------------------------------------------------------------------------------------------------------------------'
+description      = 'This is exercism-cli wrapper allowing to bulk download exercism exercises based on given conditions. \n\n' \
+                   '-------------------------------------------------------------------------------------------------------------------------\n' \
+                   '\n' \
+                   'Examples of use:\n' \
+                   '\n' \
+                   'exercism-bd -t python javascript -d easy   -->   downloads all easy exercises form python and javascript tracks \n' \
+                   '\n' \
+                   'exercism-bd -g side -s unlocked -e dummy@email.com -p dummypass   -->   downloads unlocked side exercises from all tracks \n' \
+                   '\n' \
+                   '-------------------------------------------------------------------------------------------------------------------------'
 
-tracks_help     = 'Tracks you want to download exercises from.\n' \
-                  'If not provided, all accessible exercises from all accessible tracks will be downloaded.\n '
+tracks_help      = 'Tracks you want to download exercises from.\n' \
+                   'To list all available tracks run: exercism-bd --list-tracks.\n' \
+                   'If not provided, all accessible exercises from all accessible tracks will be downloaded.\n '
 
-group_help      = f'Allowed values: {", ".join(ALLOWED_GROUPS)}.\n' \
-                  f'If not provided, all accessible exercises from all accessible groups will be downloaded.\n '
+group_help       = f'Allowed values: {", ".join(ALLOWED_GROUPS)}.\n' \
+                   f'If not provided, all accessible exercises from all accessible groups will be downloaded.\n '
 
-difficulty_help = f'Allowed values: {", ".join(ALLOWED_DIFFICULTIES)}.\n' \
-                  f'If not provided, all accessible exercises of all difficulties will be downloaded.\n '
+difficulty_help  = f'Allowed values: {", ".join(ALLOWED_DIFFICULTIES)}.\n' \
+                   f'If not provided, all accessible exercises of all difficulties will be downloaded.\n '
 
-status_help     = f'Allowed values: {", ".join(ALLOWED_STATUSES)}.\n' \
-                  f'If not provided, all accessible exercises of all statuses will be downloaded.\n '
+status_help      = f'Allowed values: {", ".join(ALLOWED_STATUSES)}.\n' \
+                   f'If not provided, all accessible exercises of all statuses will be downloaded.\n '
 
-email_help      = 'Email to log in to your exercism account.\n' \
-                  'If not provided, you won\'t be allowed to filter by status or group.\n '
+email_help       = 'Email to log in to your exercism account.\n' \
+                   'If not provided, you won\'t be allowed to filter by status or group.\n '
 
-password_help   = 'Password to log in to your exercism account.\n' \
-                  'If not provided, you won\'t be allowed to filter by status or group.\n '
+password_help    = 'Password to log in to your exercism account.\n' \
+                   'If not provided, you won\'t be allowed to filter by status or group.\n '
+
+list_tracks_help = 'Lists all currently available tracks.\n '
 
 
 parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
 
-parser.add_argument('-t', '--tracks', type=str, metavar='', required=False, nargs='*', help=tracks_help, )
-parser.add_argument('-g', '--group', type=str, metavar='', required=False, help=group_help)
-parser.add_argument('-d', '--difficulty', type=str, metavar='', required=False, help=difficulty_help)
-parser.add_argument('-s', '--status', type=str, metavar='', required=False, help=status_help)
-parser.add_argument('-e', '--email', type=str, metavar='', required=False, help=email_help)
-parser.add_argument('-p', '--password', type=str, metavar='', required=False, help=password_help)
+parser.add_argument('--list-tracks', required=False, action='store_true', help=list_tracks_help)
+
+no_auth_needed_group = parser.add_argument_group('filtering options that can be used without authentication')
+no_auth_needed_group.add_argument('-t', '--tracks', type=str, metavar='', required=False, nargs='*', help=tracks_help, )
+no_auth_needed_group.add_argument('-d', '--difficulty', type=str, metavar='', choices=ALLOWED_DIFFICULTIES, required=False, help=difficulty_help)
+
+auth_needed_group = parser.add_argument_group('filtering options that cannot be used without authentication')
+auth_needed_group.add_argument('-g', '--group', type=str, metavar='', choices=ALLOWED_GROUPS, required=False, help=group_help)
+auth_needed_group.add_argument('-s', '--status', type=str, metavar='', required=False, help=status_help)
+
+authentication_group = parser.add_argument_group('authentication')
+authentication_group.add_argument('-e', '--email', type=str, metavar='', required=False, help=email_help)
+authentication_group.add_argument('-p', '--password', type=str, metavar='', required=False, help=password_help)
 
 args = parser.parse_args()
 
 
 if __name__ == '__main__':
-    download_exercises(
-        tracks=args.tracks,
-        group=args.group,
-        difficulty=args.difficulty,
-        status=args.status,
-        email=args.email,
-        password=args.password
-    )
-
-    print('done')
+    if args.list_tracks:
+        for track in __get_all_tracks():
+            print(track)
+    else:
+        download_exercises(
+            tracks=args.tracks,
+            group=args.group,
+            difficulty=args.difficulty,
+            status=args.status,
+            email=args.email,
+            password=args.password
+        )
